29903f03646f76202b4ed9790a3889d4ef689218
munnarae <skfo789@naver.com>
2024-07-21 21:53:06 +0900

    3개 이상일 경우 더보기버튼 클릭시 무한스크롤 재생

--- src/app/hooks/useSearch.ts
+++ src/app/hooks/useSearch.ts
@@ -1,28 +1,40 @@
+// hooks/useSearch.ts
 import { useState, useEffect, useRef } from 'react';
 import { companyData } from '@/app/data/companyData';
 
-export const useSearch = () => {
+    export const useSearch = () => {
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [filteredResults, setFilteredResults] = useState<any[]>([]);
     const [showMore, setShowMore] = useState(false);
     const [hasMore, setHasMore] = useState(true);
     const loader = useRef<HTMLDivElement | null>(null);
-
     const handleSearch = (term: string) => {
         setSearchTerm(term);
 
         const results = companyData.filter(result => result.name.includes(term));
         setFilteredResults(results.slice(0, 3));
-        setHasMore(results.length > 3);
+        setHasMore(results.length > 3); //
+        setShowMore(false);
     };
 
+    const loadMore = () => {
+        setFilteredResults(prevResults => {
+            const newResults = companyData.filter(result => result.name.includes(searchTerm)).slice(prevResults.length, prevResults.length + 5);
+            if (newResults.length === 0) {
+                setHasMore(false);
+            }
+            return [...prevResults, ...newResults];
+        });
+    };
 
+    // 무한 스크롤을 처리하는 useEffect
     useEffect(() => {
         if (!showMore) return;
 
         const observer = new IntersectionObserver(
             (entries) => {
                 if (entries[0].isIntersecting && hasMore) {
+                    loadMore();
                 }
             },
             { threshold: 1 }
@@ -39,8 +51,11 @@ export const useSearch = () => {
         };
     }, [showMore, hasMore]);
 
+    const handleShowMore = () => {
+        setShowMore(true);
+        loadMore(); // Load the next set of results immediately
+    };
 
-    // 검색을 리셋하는 함수입니다.
     const handleReset = () => {
         setSearchTerm('');
         setFilteredResults([]);
@@ -53,6 +68,7 @@ export const useSearch = () => {
         hasMore,
         loader,
         handleSearch,
+        handleShowMore,
         handleReset,
     };
 };
