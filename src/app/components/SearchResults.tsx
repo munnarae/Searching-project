5e02c7bb15cad5aa970e865457e5d167d270ce9b
munnarae <skfo789@naver.com>
2024-07-21 21:25:19 +0900

    컴포넌트 데이터 훅 폴더 생성

---
+++ src/app/components/SearchResults.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const SearchResults = () => {
+    return (
+        <div>
+            SearchResults
+        </div>
+    )
+}
+
+export default SearchResults;
